8866695492bd026b7944168ec515eef90688d44e
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-04-23 13:27:30 +0100

    add guest limit for rooms

--- room.rb
+++ room.rb
@@ -1,21 +1,28 @@
 class Room
 
-  attr_reader :guest_array, :song_array
+  attr_reader :guest_array, :playlist
   def initialize
     @guest_array = []
-    @song_array = []
+    @playlist = []
+    
   end
 
   def add_guest(new_guest)
-    @guest_array << new_guest
+    if @guest_array.count < 3
+      @guest_array << new_guest
+    end
   end
 
   def remove_guest(guest_leaving)
     @guest_array.delete(guest_leaving)
   end
 
-  def add_song(new_song)
-    @song_array << (new_song)
+  def add_song_to_playlist(new_song)
+    @playlist << (new_song)
   end
+
+  
+
+
 
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -9,39 +9,42 @@ class RoomTest < Minitest::Test
     @guest1 = Guest.new("Alex", 100, "Son of a preacher man")
     @guest2 = Guest.new("Paul", 500, "All the single ladies")
     @guest3 = Guest.new("Angus", 1000, "YMCA")
+    @guest4 = Guest.new("Tristan", 200, "Sweet Caroline")
     @song1 = Song.new("YMCA")
+    @room1 = Room.new()
   end
   
   def test_can_create_rooms
-    room = Room.new
-    refute_nil(room)
+    refute_nil(@room1)
   end
 
   def test_room_is_empty
-    room = Room.new
-    assert_equal(0, room.guest_array.count)
+    assert_equal(0, @room1.guest_array.count)
   end
   
   def test_can_add_guest
-    room = Room.new
-    
-    room.add_guest(@guest1)
-    assert_equal(1, room.guest_array.count)
+    @room1.add_guest(@guest1)
+    assert_equal(1, @room1.guest_array.count)
   end
 
   def test_can_remove_guest
-    room = Room.new
-    room.add_guest(@guest1)
-    room.add_guest(@guest2)
-    room.add_guest(@guest3)
-    room.remove_guest(@guest1)
-    assert_equal(2, room.guest_array.count)
+    @room1.add_guest(@guest1)
+    @room1.add_guest(@guest2)
+    @room1.add_guest(@guest3)
+    @room1.remove_guest(@guest1)
+    assert_equal(2, @room1.guest_array.count)
   end 
 
   def test_can_add_songs
-    room = Room.new
-    room.add_song(@song1)
-    assert_equal(1, room.song_array.count)
+    @room1.add_song_to_playlist(@song1)
+    assert_equal(1, @room1.playlist.count)
   end
 
+  def test_cannot_add_guest_if_guest_array_is_equal_to_3
+    @room1.add_guest(@guest1)
+    @room1.add_guest(@guest2)
+    @room1.add_guest(@guest3)
+    @room1.add_guest(@guest4)
+    assert_equal(3, @room1.guest_array.count)
+  end
 end
