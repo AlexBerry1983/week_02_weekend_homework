5ce533fe4ef22635d7e4221e71d434494568da5d
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-04-23 15:33:51 +0100

    add rooms to karaoke_club class

--- karaoke_club.rb
+++ karaoke_club.rb
@@ -1,6 +1,13 @@
 class Karaoke_club
 
-  def initialize 
+  attr_reader :number_of_rooms
+  
+  def initialize
+    @number_of_rooms = []
+  end
+
+  def add_room(new_room)
+    @number_of_rooms << new_room
   end
 
 end
--- specs/karaoke_club_spec.rb
+++ specs/karaoke_club_spec.rb
@@ -1,14 +1,31 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../karaoke_club')
+require_relative('../room')
 
 class TestKaraoke_club < Minitest::Test
 
- def test_can_create_club
-  club = Karaoke_club.new
-  refute_nil(club)
- end
+  def setup
+    @club = Karaoke_club.new
+    @room1 = Room.new
+    @room2 = Room.new
+    @room3 = Room.new
+  end
 
+  def test_can_create_club
+    refute_nil(@club)
+  end
+
+  def test_nil_rooms_at_setup
+    @club.number_of_rooms.count
+    assert_equal(0, @club.number_of_rooms.count)
+  end
+  
+  def test_add_room
+    @club.add_room(@room1)
+    @club.add_room(@room2)
+    assert_equal(2, @club.number_of_rooms.count)
+  end
 
 
 
